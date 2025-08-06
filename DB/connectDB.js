54eb9fd845a992a771441060aada5a67ca06cb62
safvan <sanusafvanp94@gmail.com>
2025-08-07 00:17:08 +0530

    Added all files back to Git

---
+++ DB/connectDB.js
@@ -0,0 +1,13 @@
+// db.js
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/userLog');
+    console.log(' Connected to MongoDB');
+  } catch (err) {
+    console.error(' Connection error:', err);
+  }
+};
+
+module.exports = connectDB;
